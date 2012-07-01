6afeb1320fa2ebd2b845a8f9d2e886fe0e18a086
John Hopper <john.hopper@rackspace.com>
2012-07-01 00:52:32 -0500

    adding parser pooling

--- src/main/java/net/jps/nuke/atom/sax/SaxAtomParser.java
+++ src/main/java/net/jps/nuke/atom/sax/SaxAtomParser.java
@@ -1,5 +1,11 @@
 package net.jps.nuke.atom.sax;
 
+import com.rackspace.papi.commons.util.pooling.ConstructionStrategy;
+import com.rackspace.papi.commons.util.pooling.GenericBlockingResourcePool;
+import com.rackspace.papi.commons.util.pooling.Pool;
+import com.rackspace.papi.commons.util.pooling.ResourceConstructionException;
+import com.rackspace.papi.commons.util.pooling.ResourceContext;
+import com.rackspace.papi.commons.util.pooling.ResourceContextException;
 import java.io.InputStream;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -14,23 +20,40 @@ import net.jps.nuke.atom.ParserResult;
 public class SaxAtomParser implements FeedParser {
 
    private final SAXParserFactory parserFactory;
+   private final Pool<SAXParser> parserPool;
 
    public SaxAtomParser() {
       this(SAXParserFactory.newInstance());
    }
 
-   public SaxAtomParser(SAXParserFactory parserFactory) {
-      this.parserFactory = parserFactory;
+   public SaxAtomParser(SAXParserFactory parserFactoryInst) {
+      this.parserFactory = parserFactoryInst;
+      this.parserPool = new GenericBlockingResourcePool<SAXParser>(new ConstructionStrategy<SAXParser>() {
+         public SAXParser construct() throws ResourceConstructionException {
+            try {
+               return parserFactory.newSAXParser();
+            } catch (Exception ex) {
+               throw new ResourceConstructionException(ex.getMessage(), ex);
+            }
+         }
+      });
    }
 
    @Override
-   public ParserResult read(InputStream source) throws AtomParserException {
+   public ParserResult read(final InputStream source) throws AtomParserException {
       try {
-         final SAXParser parser = parserFactory.newSAXParser();
-         final AtomHandler handler = new AtomHandler(parser.getXMLReader());
-         parser.parse(source, handler);
-         
-         return handler.getResult();
+         return parserPool.use(new ResourceContext<SAXParser, ParserResult>() {
+            public ParserResult perform(SAXParser parser) throws ResourceContextException {
+               try {
+                  final AtomHandler handler = new AtomHandler(parser.getXMLReader());
+                  parser.parse(source, handler);
+
+                  return handler.getResult();
+               } catch (Exception ex) {
+                  throw new ResourceContextException(ex.getMessage(), ex);
+               }
+            }
+         });
       } catch (Exception e) {
          e.printStackTrace();
          throw new AtomParserException(e.getMessage(), e.getCause());
