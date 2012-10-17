23be8f8aa9595233bcc1964380907600337ba5ca
John Hopper <john.hopper@rackspace.com>
2012-10-17 17:32:27 -0500

    unified gc placeholder for 1.1

---
+++ support/container/src/main/java/org/atomnuke/container/gc/CancellationRemoteGarbageCollector.java
@@ -0,0 +1,10 @@
+package org.atomnuke.container.gc;
+
+/**
+ * TODO: Placeholder. I know this is bad practice. Sue me later.
+ * 
+ * @author zinic
+ */
+public class CancellationRemoteGarbageCollector {
+
+}
