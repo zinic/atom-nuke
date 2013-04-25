dacb5b38996a5c733d875a79cef5f142593a0650
John Hopper <john.hopper@rackspace.com>
2013-04-24 19:30:19 -0500

    fixing some packaging stuff

--- packaging/src/scripts/fallout.sh
+++ packaging/src/scripts/fallout.sh
@@ -6,11 +6,15 @@ export NUKE_DEPLOY_DIR="${NUKE_HOME}/deployed";
 export NUKE_LIB_DIR="${NUKE_HOME}/deployable";
 export NUKE_CONFIG_DIR="/etc/atomnuke";
 
+if [ -z "${JAVA_OPTS}" ]; then
+   JAVA_OPTS="-Xms2G -Xmx4G -XX:MaxPermSize=1G"
+fi
+
 # Location of the jar distribution
 JAR_LOCATION="/usr/lib/atomnuke/fallout-full.jar";
 
 if [ -z "${JPDA}" ]; then
-    java -XX:MaxPermSize=128M -jar "${JAR_LOCATION}" $@
+    java ${JAVA_OPTS} -jar "${JAR_LOCATION}" $@;
 else
-    java -XX:MaxPermSize=128M -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=y -jar "${JAR_LOCATION}" $@
+    java ${JAVA_OPTS} -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=y -jar "${JAR_LOCATION}" $@;
 fi
